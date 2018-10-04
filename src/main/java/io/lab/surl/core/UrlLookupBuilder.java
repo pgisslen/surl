eb67150c46a69951cf8895345b08ad59b8eabd4d
Petter Gisslen <petter@izettle.com>
2018-10-04 08:04:01 +0200

    Some more doc

--- src/main/java/io/lab/surl/core/UrlLookupBuilder.java
+++ src/main/java/io/lab/surl/core/UrlLookupBuilder.java
@@ -6,16 +6,17 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  *
- * Create short URLs.
+ * Build keys from URL.
+ *
  * Inspiration from https://blog.codinghorror.com/url-shortening-hashes-in-practice/
  *
  */
 public class UrlLookupBuilder {
 
     /**
-     * Less risk of collisions. Almost none
-     * @param url
-     * @return
+     * Less risk of collisions. Almost none.
+     * @param url URL you wan to shorten
+     * @return A pretty unique string, same result for same url
      */
     public static String withMd5(@NonNull final String url) {
 
@@ -25,8 +26,8 @@ public class UrlLookupBuilder {
     /**
      * A bit higher risk of collisions, but probably ok.
      *
-     * @param url
-     * @return
+     * @param url URL you wan to shorten
+     * @return A pretty unique string, same result for same url
      */
     public static String withCrc32(@NonNull final String url) {
 
